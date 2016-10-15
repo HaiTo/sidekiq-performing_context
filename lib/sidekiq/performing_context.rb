ce3ea674ebbbf96f102c273e1adaf7f98b07cf32
dany1468 <dany1468@gmail.com>
2016-10-15 23:26:06 +0900

    bundle gem sidekiq-performing_context

---
+++ lib/sidekiq/performing_context.rb
@@ -0,0 +1,7 @@
+require "sidekiq/performing_context/version"
+
+module Sidekiq
+  module PerformingContext
+    # Your code goes here...
+  end
+end
---
+++ lib/sidekiq/performing_context/version.rb
@@ -0,0 +1,5 @@
+module Sidekiq
+  module PerformingContext
+    VERSION = "0.1.0"
+  end
+end
