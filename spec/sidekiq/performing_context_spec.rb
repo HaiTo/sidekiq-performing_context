945a0f7891a62d6f0c0fdf680df8147961b8ac5a
dany1468 <dany1468@gmail.com>
2016-10-17 11:51:54 +0900

    Add spec

--- spec/sidekiq/performing_context_spec.rb
+++ spec/sidekiq/performing_context_spec.rb
@@ -1,11 +1,57 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe Sidekiq::PerformingContext do
-  it "has a version number" do
-    expect(Sidekiq::PerformingContext::VERSION).not_to be nil
+  class Worker
+    include Sidekiq::Worker
+
+    attr_reader :first_attr
+
+    def perform(target_id)
+
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  let!(:spy_worker) { Worker.new }
+
+  before do
+    allow(Worker).to receive(:new) { spy_worker }
+  end
+
+  example 'perform once' do
+    Worker.with_context(first_attr: 999, second_attr: 'second').perform_async(123)
+
+    Sidekiq::Worker.drain_all
+
+    expect(spy_worker.first_attr).to eq(999)
+    expect(spy_worker.instance_variable_get(:@second_attr)).to eq('second')
+
+    expect(spy_worker.instance_variable_get(:@performing_context)).to eq(first_attr: 999, second_attr: 'second')
+  end
+
+  example 'perform twice' do
+    Worker.with_context(first_attr: 999, second_attr: 'second').perform_async(123)
+
+    Sidekiq::Worker.drain_all
+
+    Worker.with_context(first_attr: 'first', second_attr: 777).perform_async(123)
+
+    Sidekiq::Worker.drain_all
+
+    expect(spy_worker.first_attr).to eq('first')
+    expect(spy_worker.instance_variable_get(:@second_attr)).to eq(777)
+
+    expect(spy_worker.instance_variable_get(:@performing_context)).to eq(first_attr: 'first', second_attr: 777)
+  end
+
+  example 'overwrite context' do
+    Worker.with_context(first_attr: 999, second_attr: 'second')
+    Worker.with_context(first_attr: 'overwrite first', second_attr: 'overwrite second').perform_async(123)
+
+    Sidekiq::Worker.drain_all
+
+    expect(spy_worker.first_attr).to eq('overwrite first')
+    expect(spy_worker.instance_variable_get(:@second_attr)).to eq('overwrite second')
+
+    expect(spy_worker.instance_variable_get(:@performing_context)).to eq(first_attr: 'overwrite first', second_attr: 'overwrite second')
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,23 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "sidekiq/performing_context"
+require 'sidekiq'
+require 'sidekiq/performing_context'
+require 'sidekiq/testing'
+require 'mock_redis'
+
+RSpec.configure do |config|
+  config.before :all do
+    Redis.current = MockRedis.new
+  end
+
+  config.before do
+    Sidekiq::Worker.clear_all
+  end
+
+  config.define_derived_metadata do |meta|
+    meta[:aggregate_failures] = true unless meta.key?(:aggregate_failures)
+  end
+end
+
+Sidekiq::Testing.server_middleware do |chain|
+  chain.add Sidekiq::PerformingContext::Middleware
+end
